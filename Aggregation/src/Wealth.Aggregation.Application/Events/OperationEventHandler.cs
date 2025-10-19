609d31310b4532c36c3fa72752f3f8cbabfedcb9
Gleb <mythoszzz@gmail.com>
2025-10-19 20:36:49 +0300

    operations handler wip

--- Aggregation/src/Wealth.Aggregation.Application/Events/OperationEventHandler.cs
+++ Aggregation/src/Wealth.Aggregation.Application/Events/OperationEventHandler.cs
@@ -1,12 +1,22 @@
+using Wealth.Aggregation.Domain;
 using Wealth.BuildingBlocks.Application;
 using Wealth.PortfolioManagement;
 
 namespace Wealth.Aggregation.Application.Events;
 
-public sealed class OperationEventHandler : IMessageHandler<OperationProto>
+public sealed class OperationEventHandler(IStockAggregationRepository repository) : IMessageHandler<OperationProto>
 {
     public Task Handle(OperationProto message, CancellationToken token)
     {
-        throw new NotImplementedException();
+        switch (message.VariantCase)
+        {
+            case OperationProto.VariantOneofCase.BondAmortizationOperation:
+            case OperationProto.VariantOneofCase.CurrencyOperation:
+            case OperationProto.VariantOneofCase.StockTrade:
+                throw new NotImplementedException();
+            case OperationProto.VariantOneofCase.None:
+            default:
+                throw new ArgumentOutOfRangeException($"Unknown variant case {message.VariantCase}");
+        }
     }
 }
