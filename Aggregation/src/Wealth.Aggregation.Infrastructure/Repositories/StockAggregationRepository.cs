76c63f2fbea25bd935b3484327897d6fcab3edc3
Gleb <mythoszzz@gmail.com>
2025-11-02 20:30:47 +0300

    migration wip

--- Aggregation/src/Wealth.Aggregation.Infrastructure/Repositories/StockAggregationRepository.cs
+++ Aggregation/src/Wealth.Aggregation.Infrastructure/Repositories/StockAggregationRepository.cs
@@ -1,97 +1,36 @@
-using Microsoft.EntityFrameworkCore;
 using SharpJuice.Clickhouse;
 using Wealth.Aggregation.Domain;
-using Wealth.Aggregation.Infrastructure.UnitOfWorks;
 using Wealth.BuildingBlocks.Domain.Common;
+using Wealth.PortfolioManagement;
 
 namespace Wealth.Aggregation.Infrastructure.Repositories;
 
-public class StockAggregationRepository(IClickHouseConnectionFactory connectionFactory) : IStockAggregationRepository
+public class StockAggregationRepository(
+    IClickHouseConnectionFactory connectionFactory,
+    ITableWriterBuilder tableWriterBuilder) : IStockAggregationRepository
 {
-    public async Task<StockAggregation?> GetStock(StockId id)
+    private readonly ITableWriter<StockTradeOperationProto> _tableWriter = tableWriterBuilder
+        .For<StockTradeOperationProto>("stock_aggregation")
+        .AddColumn("op_id", i => i.StockId.Id)
+        .AddColumn("stock_id", i => i.StockId.Id)
+        .Build();
+
+    
+    public Task Buy(string OpId, PortfolioId portfolioId, StockId stockId, long quantity, Money investment, CancellationToken token)
     {
-        return await context.StockAggregations.SingleOrDefaultAsync(i => i.Id == id);
+        await using var connection = connectionFactory.Create();
+        var command = connection.CreateCommand();
+        await connection.OpenAsync(cancellationToken);
+        connection.CreateColumnWriter()
     }
 
-    public async Task<IEnumerable<StockAggregation>> GetAggregation()
+    public Task Buy(string OpId, StockTradeOperationProto operation, CancellationToken token)
     {
-        return await context.StockAggregations.ToListAsync();
+        throw new NotImplementedException();
     }
 
-    public async Task<StockAggregation> Create(StockId id, string name, Money stockPrice, Money dividendPerYear, int lotSize)
+    public Task Sell(string OpId, StockTradeOperationProto operation, CancellationToken token)
     {
-        var stockAggregation = new StockAggregation(id, name, stockPrice, dividendPerYear, lotSize);
-        await context.StockAggregations.AddAsync(stockAggregation);
-        return stockAggregation;
-    }
-
-    public async Task ChangeName(StockId id, string name)
-    {
-        var stockAggregation = await GetStock(id);
-        if (stockAggregation != null)
-        {
-            stockAggregation.ChangeName(name);
-            await context.SaveChangesAsync();
-        }
-    }
-
-    public async Task ChangePrice(StockId id, Money price)
-    {
-        var stockAggregation = await GetStock(id);
-        if (stockAggregation != null)
-        {
-            stockAggregation.ChangePrice(price);
-            await context.SaveChangesAsync();
-        }
-    }
-
-    public async Task ChangeDividendPerYear(StockId id, Money dividendPerYear)
-    {
-        var stockAggregation = await GetStock(id);
-        if (stockAggregation != null)
-        {
-            stockAggregation.ChangeDividendPerYear(dividendPerYear);
-            await context.SaveChangesAsync();
-        }
-    }
-
-    public async Task ChangeLotSize(StockId id, int lotSize)
-    {
-        var stockAggregation = await GetStock(id);
-        if (stockAggregation != null)
-        {
-            stockAggregation.ChangeLotSize(lotSize);
-            await context.SaveChangesAsync();
-        }
-    }
-
-    public async Task Buy(PortfolioId portfolioId, StockId stockId, int quantity, Money investment, CancellationToken cancellationToken)
-    {
-        var stockAggregation = await GetStock(stockId);
-        if (stockAggregation != null)
-        {
-            stockAggregation.Buy(quantity, investment);
-            await context.SaveChangesAsync();
-        }
-    }
-
-    public async Task Sell(PortfolioId portfolioId, StockId stockId, int quantity, Money profit, CancellationToken cancellationToken)
-    {
-        var stockAggregation = await GetStock(stockId);
-        if (stockAggregation != null)
-        {
-            stockAggregation.Sell(quantity, profit);
-            await context.SaveChangesAsync();
-        }
-    }
-
-    public async Task AddDividend(StockId id, Money dividend)
-    {
-        var stockAggregation = await GetStock(id);
-        if (stockAggregation != null)
-        {
-            stockAggregation.AddDividend(dividend);
-            await context.SaveChangesAsync();
-        }
+        throw new NotImplementedException();
     }
 }
