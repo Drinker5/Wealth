433b785addc49fbdb11ad4cd4fd366b934e362a9
Gleb <mythoszzz@gmail.com>
2025-11-08 10:23:45 +0300

    add fixture

---
+++ Aggregation/tests/Wealth.Aggregation.Infrastructure.Tests/Fixture.cs
@@ -0,0 +1,24 @@
+﻿using Testcontainers.ClickHouse;
+
+namespace Wealth.Aggregation.Infrastructure.Tests;
+
+public sealed class Fixture : IAsyncLifetime
+{
+    private readonly ClickHouseContainer clickHouseContainer = new ClickHouseBuilder()
+        .Build();
+
+    public Fixture()
+    {
+    }
+
+    public async Task InitializeAsync()
+    {
+        await clickHouseContainer.StartAsync();
+    }
+
+
+    public async Task DisposeAsync()
+    {
+        await clickHouseContainer.StopAsync();
+    }
+}
