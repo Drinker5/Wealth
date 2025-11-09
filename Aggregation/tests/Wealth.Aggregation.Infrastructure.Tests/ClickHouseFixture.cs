950d09cb68063e1520e96754dc56732bebcd2647
Gleb <mythoszzz@gmail.com>
2025-11-09 20:21:39 +0500

    testcontainers clickhouse usage

---
+++ Aggregation/tests/Wealth.Aggregation.Infrastructure.Tests/ClickHouseFixture.cs
@@ -0,0 +1,38 @@
+﻿using Testcontainers.ClickHouse;
+
+namespace Wealth.Aggregation.Infrastructure.Tests;
+
+public sealed class ClickHouseFixture : IAsyncLifetime
+{
+    private readonly ClickHouseContainer clickHouseContainer = new ClickHouseBuilder()
+        .WithDatabase("default")
+        .WithUsername("default")
+        .WithPassword("default")
+        .Build();
+
+    public async Task InitializeAsync()
+    {
+        await clickHouseContainer.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await clickHouseContainer.DisposeAsync();
+    }
+
+    public string ClickHouseConnectionString
+    {
+        get
+        {
+            var properties = new Dictionary<string, string>
+            {
+                { "Host", clickHouseContainer.Hostname },
+                { "Port", clickHouseContainer.GetMappedPublicPort(ClickHouseBuilder.NativePort).ToString() },
+                { "Database", "default" },
+                { "User", "default" },
+                { "Password", "default" }
+            };
+            return string.Join(";", properties.Select(property => string.Join("=", property.Key, property.Value)));
+        }
+    }
+}
