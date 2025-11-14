eb60ec83018ae835dcfaf2c3fd0790db70895685
Gleb <mythoszzz@gmail.com>
2025-11-14 21:42:05 +0500

    goose migration in testcontainer

--- Aggregation/tests/Wealth.Aggregation.Infrastructure.Tests/ClickHouseFixture.cs
+++ Aggregation/tests/Wealth.Aggregation.Infrastructure.Tests/ClickHouseFixture.cs
@@ -1,22 +1,53 @@
-﻿using Testcontainers.ClickHouse;
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Containers;
+using Testcontainers.ClickHouse;
 
 namespace Wealth.Aggregation.Infrastructure.Tests;
 
 public sealed class ClickHouseFixture : IAsyncLifetime
 {
-    private readonly ClickHouseContainer clickHouseContainer = new ClickHouseBuilder()
-        .WithDatabase("default")
-        .WithUsername("default")
-        .WithPassword("default")
-        .Build();
+    private readonly ClickHouseContainer clickHouseContainer;
+
+    public readonly IContainer migratorContainer;
+
+    public ClickHouseFixture()
+    {
+        var network = new NetworkBuilder()
+            .Build();
+        
+        clickHouseContainer = new ClickHouseBuilder()
+            .WithNetworkAliases("clickhouse")
+            .WithNetwork(network)
+            .WithDatabase("default")
+            .WithUsername("default")
+            .WithPassword("default")
+            .Build();
+        
+        migratorContainer = new ContainerBuilder()
+            .WithNetwork(network)
+            .WithImage("gomicro/goose:3.26.0")
+            .WithBindMount(Path.Combine(Directory.GetCurrentDirectory(), "Migrations"), "/migrations", AccessMode.ReadOnly)
+            .DependsOn(clickHouseContainer)
+            .WithEnvironment(new Dictionary<string, string>
+            {
+                { "GOOSE_DRIVER", "clickhouse" },
+                { "GOOSE_DBSTRING", "tcp://default:default@clickhouse:9000" },
+                { "GOOSE_MIGRATION_DIR", "." },
+            })
+            .WithCommand("sh", "-c", "goose up")
+            .Build();
+    }
 
     public async Task InitializeAsync()
     {
         await clickHouseContainer.StartAsync();
+        await migratorContainer.StartAsync();
     }
 
     public async Task DisposeAsync()
     {
+        await migratorContainer.DisposeAsync(); 
         await clickHouseContainer.DisposeAsync();
     }
 
