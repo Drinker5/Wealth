130553f905543d841840e815db607271c4c9e56d
Gleb <mythoszzz@gmail.com>
2025-11-17 22:01:43 +0500

    currency operation handle

--- Aggregation/src/Wealth.Aggregation.Infrastructure/Migrations/01_init.sql
+++ Aggregation/src/Wealth.Aggregation.Infrastructure/Migrations/01_init.sql
@@ -14,15 +14,28 @@ CREATE TABLE IF NOT EXISTS stock_trade
 
 CREATE TABLE IF NOT EXISTS bond_coupon
 (
-    op_id    String,
-    date     DateTime,
+    op_id        String,
+    date         DateTime,
     portfolio_id Int32,
-    bond_id  Int32,
-    amount   Decimal(18, 2),
-    currency UInt8
+    bond_id      Int32,
+    amount       Decimal(18, 2),
+    currency     UInt8
+) ENGINE = ReplacingMergeTree
+      ORDER BY (op_id);
+
+CREATE TABLE IF NOT EXISTS currency_operation
+(
+    op_id        String,
+    date         DateTime,
+    portfolio_id Int32,
+    amount       Decimal(18, 2),
+    currency     UInt8,
+    type         UInt8
+
 ) ENGINE = ReplacingMergeTree
       ORDER BY (op_id);
 
 -- +goose down
 DROP TABLE IF EXISTS stock_trade;
 DROP TABLE IF EXISTS bond_coupon;
+DROP TABLE IF EXISTS currency_operation;
